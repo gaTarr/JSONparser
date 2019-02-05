1b520c236cd077dc4cd294962403475c5c69f625
Greg Tarr <gregory.a.tarr@gmail.com>
2019-02-05 01:06:07 -0600

    Added Album Input Validation
    Album selections of less than one, or greater than 100 will not be accepted.

--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -6,47 +6,40 @@ import java.util.Scanner;
  */
 
 /**
- * @author Greg Tarr
+ * @author Greg Tarr 
  * Provides interaction with application user.
  *
  */
 public class UserInput {
 
 	/**
-	 * @return 
-	 * Provides album number so main method knows where to 
-	 * process JSON from. Prompts user to select which album 
-	 * they want to view and returns result to Main method 
-	 * as a string value.  
+	 * @return Provides album number so main method knows where to process JSON
+	 *         from. Prompts user to select which album they want to view and
+	 *         returns result to Main method as a string value.
 	 */
 	public static String getAlbum() {
 
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
-
-		System.out.print("Enter the number of the album you wish to view (1-100): ");
-
 		int input = 0;
-		boolean validInput = false;
-		while (!validInput) {
+		do {
+			System.out.print("Enter the number of the album you wish to view (1-100): ");
 			try {
 				input = in.nextInt();
-				validInput = true;
-
+				if (input < 1 || input > 100) {
+					System.out.println("  ***Invalid Selection - Selection must be between 1 and 100***");
+				}
 			} catch (InputMismatchException e) {
-				System.out.println("  ***Invalid Selection***");
-				System.out.print("You must select a valid album number between 1-100: ");
-				in.nextLine();
+				System.out.println("  ***Invalid Selection - You must enter an integer***");
+				in.next();
 			}
-		}
+		} while (input < 1 || input > 100);
 		return Integer.toString(input);
 	}
 
 	/**
-	 * @return
-	 * Provides boolean value so Main method knows to stop
-	 * or continue.  Prompts the user for input, and returns
-	 * boolean value.  
+	 * @return Provides boolean value so Main method knows to stop or continue.
+	 *         Prompts the user for input, and returns boolean value.
 	 */
 	public static boolean finished() {
 
@@ -59,7 +52,7 @@ public class UserInput {
 		String response = "";
 		boolean selection = false;
 		boolean validInput = false;
-		
+
 		while (!validInput) {
 			response = in.nextLine();
 			if (response.equalsIgnoreCase("yes")) {
